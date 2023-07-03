b26f1985ea49f279d05328a908d4fc57d52332d8
Pretty Nonhlanhla Kunene <32060984+nonoza@users.noreply.github.com>
2023-07-03 16:28:08 +0200

    Update script.js

--- ping-coming-soon-page-master/js/script.js
+++ ping-coming-soon-page-master/js/script.js
@@ -3,8 +3,19 @@ const pattens = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
 const email = document.forms["signup"]["email"].value;
 const form = document.getElementById("form");
 const emailborder = document.querySelector("#email");
+const errorMessage = document.querySelector('.error');
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
   
   });
+
+  submitBtn.addEventListener('click',(w) => {
+    if(email == null || email == undefined) {
+    errorMessage.innerHTML = "Please provide an email";
+    emailborder.style.border = "1px solid red";
+    }
+    if (email != pattens) {
+      errorMessage.innerHTML = "Please provide a valid email address";
+    }
+  })
