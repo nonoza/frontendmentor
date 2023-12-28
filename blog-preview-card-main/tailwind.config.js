f0821d17d851b652429be3b5fa79088340a88632
Pretty Nonhlanhla Kunene <32060984+nonoza@users.noreply.github.com>
2023-12-28 23:00:32 +0200

    Add files via upload

---
+++ blog-preview-card-main/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    backgroundColor: {
+     "blog-yellow" : "hsl(47, 88%, 63%)",
+     "blog-white": "hsl(0, 0%, 100%)"
+    },
+    colors: {
+      "blog-grey" : "hsl(0, 0%, 50%)",
+      "blog-black" : "hsl(0, 0%, 7%)"
+    },
+    fontSize: {
+      "blog-body" : "14px",
+      "blog-heading" : "23px",
+    
+    },
+    fontFamily: {
+      "blog-font-family" : ['Figtree', 'sans-serif']
+    },
+    boxShadow: {
+      "blog-box-shadow": "10px 9px hsl(0, 0%, 7%);"
+    }
+   },
+  plugins: [],
+}
