7ece316a60f156ea3ee2ca47bb43e9da4d00b518
Pretty Nonhlanhla Kunene <32060984+nonoza@users.noreply.github.com>
2024-06-10 22:23:57 +0200

    modifying the designs

--- expenses-chart-component-main/js/script.js
+++ expenses-chart-component-main/js/script.js
@@ -1,10 +1,33 @@
-let chatBtn = document.querySelectorAll(".week");
+let chatBtns = document.querySelectorAll(".week");
 
-Array.from(chatBtn).forEach(function (el) {
-  el.addEventListener("mouseover", function () {
-    let amount = document.querySelectorAll(".amount");
-    for (var i = 0; i < amount.length; i++) {
-      amount[i].style.display = "block";
+if (chatBtns) {
+  chatBtns.forEach((chat, index) => {
+    if (chat !== undefined) {
+      chat.addEventListener("mouseover", function() {
+        let amounts = document.querySelectorAll(".amount");
+        if (amounts[index]) {
+          amounts[index].style.opacity = "1";
+          amounts[index].style.visibility = "visible";
+        }
+      });
+
+      chat.addEventListener("mouseout", function() {
+        let amounts = document.querySelectorAll(".amount");
+        if (amounts[index]) {
+          amounts[index].style.opacity = "0";
+          amounts[index].style.visibility = "hidden";
+        }
+      });
     }
   });
-});
+}
+
+
+// Array.from(chatBtn).forEach(function (el) {
+//   el.addEventListener("mouseover", function () {
+//     let amount = document.querySelectorAll(".amount");
+//     for (var i = 0; i < amount.length; i++) {
+//       amount[i].style.display = "block";
+//     }
+//   });
+// });
