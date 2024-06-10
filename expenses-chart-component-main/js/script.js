9b2fc5e7158dd2276ad3b9881ea99d59ec915aa0
Pretty Nonhlanhla Kunene <32060984+nonoza@users.noreply.github.com>
2024-06-10 21:33:31 +0200

    Update script.js

--- expenses-chart-component-main/js/script.js
+++ expenses-chart-component-main/js/script.js
@@ -2,9 +2,16 @@ let chatBtn = document.querySelectorAll(".week");
 
 Array.from(chatBtn).forEach(function (el) {
   el.addEventListener("mouseover", function () {
-    let amount = document.querySelectorAll(".amount");
-    for (var i = 0; i < amount.length; i++) {
-      amount[i].style.display = "block";
+    let amount = el.querySelector(".amount");
+    if (amount) {
+      amount.style.display = "block";
+    }
+  });
+
+  el.addEventListener("mouseout", function () {
+    let amount = el.querySelector(".amount");
+    if (amount) {
+      amount.style.display = "none";
     }
   });
 });
