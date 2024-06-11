fa848ceed4ac6baa3a6f117a790361b172d4d96d
Pretty Nonhlanhla Kunene <32060984+nonoza@users.noreply.github.com>
2024-06-11 11:07:00 +0200

    modifiying the project

--- interactive-rating-component-main/js/script.js
+++ interactive-rating-component-main/js/script.js
@@ -5,24 +5,42 @@ let selectedNumber = document.querySelector('span');
 let ratingSection = document.querySelectorAll('section.front-container');
 let thankSection = document.querySelectorAll('section.back-container');
 
-// Adding click event listeners to each rating number element.
-ratingNumber.forEach((number) => {
-    number.addEventListener("click", () => {
-        console.log("number correct"); // Logging to the console when a rating number is clicked.
-        selectedNumber.innerHTML = number.innerHTML; // Setting the selected number in the span.
-    });
-});
+// // Adding click event listeners to each rating number element.
+// ratingNumber.forEach((number) => {
+//     number.addEventListener("click", () => {
+//         console.log("number correct"); // Logging to the console when a rating number is clicked.
+//         selectedNumber.innerHTML = number.innerHTML; // Setting the selected number in the span.
+//     });
+// });
 
-// Adding a click event listener to the submit button.
-submitBtn.addEventListener('click', function () {
-    // Checking the display property to toggle between the rating and thank you sections.
-    if (ratingSection[0].style.display === "block") {
-        ratingSection[0].style.display = "none";
-        thankSection[0].style.display = "block";
-    } else {
-        thankSection[0].style.display = "none";
-        ratingSection[0].style.display = "block";
-    }
-});
+// // Adding a click event listener to the submit button.
+// submitBtn.addEventListener('click', function () {
+//     // Checking the display property to toggle between the rating and thank you sections.
+//     if (ratingSection[0].style.display === "block") {
+//         ratingSection[0].style.display = "none";
+//         thankSection[0].style.display = "block";
+//     } else {
+//         thankSection[0].style.display = "none";
+//         ratingSection[0].style.display = "block";
+//     }
+// });
 
+
+ratingNumber.forEach((number) =>{
+    number.addEventListener("click", () =>{
+        console.log("number correct");
+        selectedNumber.innerHTML = number.innerHTML;
+        ratingNumber.forEach((num) => num.classList.remove('active')); // Remove active class from all
+        number.classList.add('active'); // Add active class to the clicked one
+        submitBtn.addEventListener('click',function(){
+            if (ratingSection[0].style.display = "block"){
+                ratingSection[0].style.display = "none";
+                thankSection[0].style.display = "block";
+            } else {
+                 thankSection[0].style.display = "none";
+                 ratingSection[0].style.display = "block";
+            }
+        })
+    })
+})
 AOS.init();
